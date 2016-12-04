68eb75dcd7365ed597baa8e591cd5a59d3adc5a7
yuuichi.nagakura <nagakuray@nagakurayuuichi-no-MacBook-Air.local>
2016-12-04 19:52:10 +0900

    Implementation of the validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ActiveRecord::Base
+	#　名前は20文字以内
+	validates :name ,length: {maximum: 20} , presence: true
+	# 内容は必須かつ2文字以上、30文字以内
+	validates :body, length: {minimum: 2, maximum: 30}, presence: true
 end
