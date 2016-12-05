1d68621fece177cf595dde3627c79e7b798770fc
yuuichi.nagakura <nagakuray@nagakurayuuichi-no-MacBook-Air.local>
2016-12-05 11:29:48 +0900

    challenge2 submit

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
 	validates :name ,length: {maximum: 20} , presence: true
 	# 内容は必須かつ2文字以上、30文字以内
 	validates :body, length: {minimum: 2, maximum: 30}, presence: true
+	# 年齢は0以上かつ150以下の数字
+	validates :age, numericality:{only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 150}, presence: true
 end
